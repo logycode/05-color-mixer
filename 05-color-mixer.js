484647c22217d8e921b15a3abdcbe0ee7c239fe0
Tim Kranich <t.kranich@reply.de>
2023-01-21 16:21:15 +0100

    adds init function and changes eventListener to be more dynamic

--- 05-color-mixer.js
+++ 05-color-mixer.js
@@ -1,32 +1,29 @@
-// red init
-let red = 47;
-const redText = document.createTextNode(red);
-document.querySelector(".redOutput").appendChild(redText);
-document.querySelector("#red").value = red;
+const colorNames = ["red", "green", "blue"];
+const initialColors = [47,79,79];
 
-// green init
-let green = 79;
-const greenText = document.createTextNode(green);
-document.querySelector(".greenOutput").appendChild(greenText);
-document.querySelector("#green").value = green;
+function init(colorArray) {
+    colorNames.forEach((colorName, index) => {
+        const text = document.createTextNode(colorArray[index]);
+        document.querySelector("."+colorName).appendChild(text);
+        document.querySelector("#"+colorName).value = colorArray[index]; 
+    })
+}
 
-// blue init
-let blue = 79;
-const blueText = document.createTextNode(blue);
-document.querySelector(".blueOutput").appendChild(blueText);
-document.querySelector("#blue").value = blue;
+init(initialColors)
 
 document.addEventListener("input", event => {
-    switch (event.target.id) {
-        case "red": red = event.target.value; break;
-        case "green": green = event.target.value; break;
-        case "blue": blue = event.target.value; break;
-    }
+    const value = event.target.value;
+    const reference = event.target.id;
+    let rgb = "rgb("
 
-    rgb = "rgb(" + red + "," + green + "," + blue + ")";
+    document.querySelector("."+reference).innerText = value;
+    colorNames.forEach((colorName,index) => {
+        const color = document.querySelector("#"+colorName).value;
+        rgb += color
+        if(index < 2) {
+            rgb += ","
+        }
+    })
+    rgb += ")";
     document.querySelector("body").style.backgroundColor = rgb;
-    
-    document.querySelector(".redOutput").innerText = red;
-    document.querySelector(".greenOutput").innerText = green;
-    document.querySelector(".blueOutput").innerText = blue;
 })
